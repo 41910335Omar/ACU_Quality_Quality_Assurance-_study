02a47606ed52553aa283bf9e4a324802386b4fd8
Your Name <you@example.com>
2022-12-21 16:49:05 +0200

    change id

--- Code/code 1 .py
+++ Code/code 1 .py
@@ -1,7 +1,3 @@
-print("my name is hosam ")
-print("my id is 12345")
-print("my hight is 1223")
-print("hiii omar")
-print("my age is 12")
-print("hello world ")
-print('g')
+print("omar ")
+print("my id 41910335")
+
